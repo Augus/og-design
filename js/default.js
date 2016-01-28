c0da37e882404f251d1b93c1b764136bc16fa2ef
Augus.Chen <augus@ogdesign.tw>
2016-01-28 22:05:00 +0800

    作品查看方式改用動態產生、支援鍵盤事件、新增載入動畫提示

--- js/default.js
+++ js/default.js
@@ -1,7 +1,170 @@
-/* ==========================================================
-  Base Default JavaScript
-  -- Table of Contents --
-*/
+var app = angular.module("ogdesign", ['angular-google-analytics']);
+
+app.config(function(AnalyticsProvider) {
+    AnalyticsProvider.setAccount([{
+        tracker: 'UA-52653674-2',
+        name: "tracker"
+    }]);
+});
+
+app.directive('mousetrap', function () {
+    return {
+        restrict: 'A',
+        controller: ['$scope', '$element', '$attrs',
+                     function ($scope, $element, $attrs) {
+            
+            var mousetrap;
+
+            $scope.$watch($attrs.mousetrap, function(_mousetrap) {
+                mousetrap = _mousetrap;
+
+                for (var key in mousetrap) {
+                    if (mousetrap.hasOwnProperty(key)) {
+                        Mousetrap.unbind(key);
+                        Mousetrap.bind(key, applyWrapper(mousetrap[key])); 
+                    }
+                }
+            }, true);
+            
+            function applyWrapper(func) {
+                return function(e) {
+                    $scope.$apply(function() {
+                        func(e);
+                    });
+                };
+            }
+            
+            $element.bind('$destroy', function() {
+                if (!mousetrap) return;
+
+                for (var key in mousetrap) {
+                    if (mousetrap.hasOwnProperty(key)) {
+                        Mousetrap.unbind(key);
+                    }
+                }
+            });
+
+        }]
+    }
+});
 
 
-// JS functions and initiations go here...
+app.controller("RootController", function($scope, $timeout, Analytics) {
+
+    $scope.isOpenPlayer = false;
+
+    $scope.openPlayer = function(portfolio, index) {
+        $scope.isOpenPlayer = true;
+        $scope.goto(portfolio, index);
+    };
+
+    $scope.closePlayer = function() {
+        $scope.isOpenPlayer = false;
+        $scope.current = undefined;
+    };
+
+    $scope.goto = function (portfolio, index) {
+        Analytics.trackEvent('Portfolio', 'View', portfolio.name);
+        if ($scope.current) {
+            $scope.current.scrollTop = $("#player").scrollTop();
+        }
+        $scope.currentIndex = index;
+        $scope.current = portfolio;
+        $scope.isSmall = portfolio.isSmall;
+        $("#player").scrollTop($scope.current.scrollTop || 0);
+    };
+
+    $scope.next = function () {
+        if ($scope.currentIndex + 1 <= $scope.porfolios.length - 1) {
+            $scope.goto($scope.porfolios[$scope.currentIndex + 1], $scope.currentIndex + 1);
+        }
+        else {
+            $scope.goto($scope.porfolios[0], 0);
+        }
+    };
+
+    $scope.prev = function () {
+        if ($scope.currentIndex - 1 > 0) {
+            $scope.goto($scope.porfolios[$scope.currentIndex - 1], $scope.currentIndex - 1);
+        }
+        else {
+            $scope.goto($scope.porfolios[$scope.porfolios.length - 1], $scope.porfolios.length - 1);
+        }
+    };
+
+    // 作品
+    $scope.porfolios = [
+        {
+            thumbnail: "cupoy_web.jpg",
+            full: "cupoy_web.jpg",
+            name: "Cupoy - 網站設計",
+            isSmall: false
+        },
+        {
+            thumbnail: "cupoy_ios.jpg",
+            full: "cupoy_ios.jpg",
+            name: "Cupoy - 傳遞知識的最佳解決方案",
+            isSmall: false
+        },
+        {
+            thumbnail: "cupoy_ios_extension.jpg",
+            full: "cupoy_ios_extension.jpg",
+            name: "Cupoy - iOS Extension Design",
+            isSmall: false
+        },
+        {
+            thumbnail: "cupoy_email_template.jpg",
+            full: "cupoy_email_template.jpg",
+            name: "CUPOY - Email Template Design",
+            isSmall: false
+        },
+        {
+            thumbnail: "smarttrip.jpg",
+            full: "smarttrip.jpg",
+            name: "台灣智慧觀光",
+            isSmall: false
+        },
+        {
+            thumbnail: "clipo.jpg",
+            full: "clipo.jpg",
+            name: "Clipo - 收藏、整理有趣的資訊",
+            isSmall: false
+        },
+        {
+            thumbnail: "金色三麥.jpg",
+            full: "金色三麥.jpg",
+            name: "金色三麥",
+            isSmall: false
+        },
+        {
+            thumbnail: "oodeco.jpg",
+            full: "oodeco.jpg",
+            name: "ooDesign",
+            isSmall: false
+        },
+        {
+            thumbnail: "豐盛.jpg",
+            full: "豐盛.jpg",
+            name: "豐盛智匯",
+            isSmall: false
+        },
+        {
+            thumbnail: "CUPOYV5.jpg",
+            full: "CUPOYV5.jpg",
+            name: "CUPOY alpha",
+            isSmall: false
+        },
+        {
+            thumbnail: "Jibaoapp.jpg",
+            full: "Jibaoapp.jpg",
+            name: "吉寶知識系統",
+            isSmall: true
+        },
+        {
+            thumbnail: "AIESEC.jpg",
+            full: "AIESEC.jpg",
+            name: "AIESEC Taiwan",
+            isSmall: true
+        },
+    ];
+});
